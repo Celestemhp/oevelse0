fb6e443cae0452a58a22240422d9c6e184692123
Celeste Michelle <cele0216@stud.kea@purplegraphics.dk>
2022-08-22 12:29:14 +0200

    4

--- script.js
+++ script.js
@@ -3,22 +3,22 @@ const snaps = document.querySelector("#snaps");
 const faxe = document.querySelector("#faxe");
 const sodavand = document.querySelector("#sodavand");
 
-  
-  
-  //Lyt efter klik på alle elementer
-  øl.addEventListener("mousedown", klik_øl);
-  snaps.addEventListener("mousedown", klik_snaps);
-  faxe.addEventListener("mousedown", klik_faxe);
-  sodavand.addEventListener("mousedown", klik_sodavand);
+
+
+//Lyt efter klik på alle elementer
+øl.addEventListener("mousedown", klik_øl);
+snaps.addEventListener("mousedown", klik_snaps);
+faxe.addEventListener("mousedown", klik_faxe);
+sodavand.addEventListener("mousedown", klik_sodavand);
 
 
 
   function klik_øl() {
     document.getElementById("#oel.webp").click();
-    if onclick {
+    if (onclick) {
         console.log("indeholder alkohol");
     }
     else {
-
+        console.log("indeholder alkohol");
     }
   } 
